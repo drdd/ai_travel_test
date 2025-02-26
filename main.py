ea2fcbb96b0367ed224ab5cc26c63ad57732d4c6
drdd <mymailbyte@gmail.com>
2025-02-26 19:45:13 +0300

    added get last msg endpoint

--- main.py
+++ main.py
@@ -22,11 +22,38 @@ def get_thread():
             except Exception as e:
                 return jsonify({"msg": "Неверный api_key"}), 400
 
+@app.route('/g', methods=['POST'])
+def get_message():
+    """
+    Получает последнее сообщение по thread_id POST /g.
+
+    Ожидаемые параметры JSON:
+    - api_key (str): API ключь для OpenAI. Обязательный.
+    - assistant_id (str): ASSISTANT ID для OpenAI. Обязательный.
+    - thread_id (str): thread_id для OpenAI. Обязательный.
+    """
+    if request.is_json:
+        data = request.get_json()
+        if "assistant_id" in data and "api_key" in data and "thread_id" in data:
+            try:
+                client = make_client_openai(data['api_key'])
+                assistant = make_assistant_openai(client, data['assistant_id'])
+                thread_id = get_exists_thread_id(client, data['thread_id'])
+                if thread_id == "":
+                    return jsonify({"msg": f"Ошибка потока OpenAI"}), 400
+                answer = get_response(client, assistant, thread_id)
+                return jsonify({"msg": f"{answer}", "thread_id": f"{thread_id}"}), 200
+            except Exception as e:
+                return jsonify({"msg": f"Ошибка {e}"}), 400
+        else:
+            return jsonify({"msg": "Недостатачный пакет данных в запросе"}), 400
+    else:
+        return jsonify({"msg": "Данные не в формате JSON"}), 400
 
 @app.route('/q', methods=['POST'])
 def send_message():
     """
-    Обрабатывает POST запросы на /q.
+    Отправляет запрос на добавление вопроса к OpenAI /q.
 
     Ожидаемые параметры JSON:
     - api_key (str): API ключь для OpenAI. Обязательный.
@@ -47,8 +74,8 @@ def send_message():
                     if thread_id == "":
                         return jsonify({"msg": f"Ошибка потока OpenAI"}), 400
 
-                    answer = assistent_queary(client, assistant, thread_id, data['msg'])
-                    return jsonify({"msg": f"{answer}", "thread_id": f"{thread_id}"}), 200
+                    assistent_queary(client, assistant, thread_id, data['msg'])
+                    return jsonify({"msg": "ok", "thread_id": f"{thread_id}"}), 200
                 except Exception as e:
                     return jsonify({"msg": f"Ошибка {e}"}), 400
         else:
@@ -78,25 +105,29 @@ def get_exists_thread_id(client, thread_id):
             return ""
         return ""
 
-def assistent_queary(client, assistant, thread_id, message):
+def assistent_queary(client, thread_id, message):
     client.beta.threads.messages.create(
         thread_id=thread_id,
         role="user",
         content=message
     )
 
-    run = client.beta.threads.runs.create_and_poll(
+def get_response(client, assistant, thread_id):
+    run = client.beta.threads.runs.create(
         thread_id=thread_id,
         assistant_id=assistant.id,
-        instructions="",
+        instructions=""
     )
+
+    while run.status != "completed":
+        run = client.beta.threads.runs.retrieve(run_id=run.id)
 
     if run.status == "completed":
         messages = client.beta.threads.messages.list(thread_id=thread_id)
-
         for msg in messages:
-            assert msg.content[0].type == "text"
-            return msg.content[0].text.value
+            if msg.content[0].type == "text":
+                return msg.content[0].text.value
+
 
 
 if __name__ == '__main__':
